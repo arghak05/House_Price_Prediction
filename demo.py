dbc7c68f3c9dc3bb914bdaf2d6ecb68d14e63057
arghak05 <arghakarmakar05@gmail.com>
2022-07-12 13:56:32 +0530

    updated

--- demo.py
+++ demo.py
@@ -6,8 +6,10 @@ import os,sys
 def main():
     try:
         config_path = os.path.join("config","config.yaml")
-        pipeline = Pipeline(Configuartion(config_file_path=config_path))
-        pipeline.run_pipeline()
+        #pipeline = Pipeline(Configuartion(config_file_path=config_path))
+        #pipeline.run_pipeline()
+        data_validation_config = Configuartion().get_data_validation_config()
+        print(data_validation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
